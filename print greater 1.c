f47400d98077674629874caa033f123891f82b96
sajidahmed999 <54901609+sajidahmed999@users.noreply.github.com>
2019-09-05 19:00:12 +0530

    Add files via upload

---
+++ print greater 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+     if(a>b)
+     {
+         printf("a is greater");
+     }
+
+     else
+     {
+         printf("b is greater");
+     }
+   getch();
+}
+
+
+
